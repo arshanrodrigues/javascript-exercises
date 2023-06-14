0269e414df2decdc6c6397b3a2b76681c0338d62
arshanrodrigues <arshanevra3@gmail.com>
2023-06-15 01:32:09 +0530

    soln added

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius= (fahrenheit-32)* 5/9;
+  const rounded_celcius= parseFloat(celcius.toFixed(1));
+  return rounded_celcius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = (celcius* 9/5 )+32;
+  const rounded_fahrenheit = parseFloat(fahrenheit.toFixed(1));
+  return rounded_fahrenheit;
+
 };
 
 // Do not edit below this line
